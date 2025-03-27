904faac6d7f82918f2aae4f0cca153a884cfc262
Harshil Gupta <harshilgupta.email@gmail.com>
2025-03-27 11:42:57 +0530

    Completed the first 2 steps of workflow Taking the source file and breaking it into chunks of texts

---
+++ chatbot.py
@@ -0,0 +1,31 @@
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+# Upload PDF files
+
+st.header("My first ChatBot")
+
+with st.sidebar:
+    st.title("Your Documents")
+    file = st.file_uploader("Upload a PDF file and start asking questions", type="pdf")
+    
+    
+#Extract the text
+if file is not None:
+    pdf_reader = PdfReader(file)
+    text = ""
+    for page in pdf_reader.pages:
+        text += page.extract_text()
+        # st.write(text)
+
+#Break it into chunks
+    text_splitter = RecursiveCharacterTextSplitter(
+        separators= "\n",
+        chunk_size= 1000,
+        chunk_overlap= 150,
+        length_function=len
+    )
+    
+    chunks = text_splitter.split_text(text)
+    st.write(chunks)
